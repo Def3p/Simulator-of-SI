aed4156aefb2329404f47d9d95a94af9b562fb6d
Def3P <122175474+Def3p@users.noreply.github.com>
2024-12-05 01:35:49 +0400

    Edit Main script

--- project/main.py
+++ project/main.py
@@ -17,21 +17,35 @@ def InitPopulation():
 def ChanceOfError(): 
     return random.randrange(101)
 
+# def ShuffleList():
+#     shuffle_list = random.shuffle(population_list)
+#     get_list = []
+#     for i in range(1, int(current_population / 2)):
+#         get_list.append(shuffle_list[i])
+#     return get_list
+    
 def MixingPopulation():
-    while(len(get_mixing_list) < current_population):
-        get_mix = random.randrange(0, current_population - 1)
-        while(len(get_mixing_list) == get_mix): 
-            get_mix = random.randrange(0, current_population - 1)
-        get_mixing_list.append(get_mix)
+    get_list = []
+    for i in range(1, int(current_population / 2) + 1):
+        get_list.append(i)
+    random.shuffle(get_list)
+    return get_list
+
+    # while(len(get_mixing_list) < current_population):
+    #     get_mix = random.randrange(0, current_population - 1)
+    #     while(len(get_mixing_list) == get_mix): 
+    #         get_mix = random.randrange(0, current_population - 1)
+    #     get_mixing_list.append(get_mix)
             
 
 # Init population
 InitPopulation()
 
-MixingPopulation()
+get_mixing_list = MixingPopulation()
 
 print(get_mixing_list)
-print(len(population_list))
+print(population_list)
+
 # fig, ax = plt.subplots()             # Create a figure containing a single Axes.
 # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
 # plt.show()                           # Show the figure.
